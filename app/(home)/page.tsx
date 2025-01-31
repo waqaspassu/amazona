de6df31dd86fc94bda16a0263c4a92743e1c43d1
waqaspassu <waqasquba@gmail.com>
2025-01-31 13:01:03 +0500

    removed errors

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,5 +2,5 @@ import { HomeCarousel } from '@/components/shared/home/home-carousel'
 import data from '@/lib/data'
 
 export default function Page() {
-  return <HomeCarousel items={data.carousels} />
+  return <HomeCarousel items={data.carousel} />
 }
